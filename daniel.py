a031f1567aacc26ac22f7be6a0609fa2d2d9fb36
danielandhika <danielandhika15@gmail.com>
2021-04-17 22:44:18 +0700

    Menuliskan Nama dan NIM anggota Kelompok

---
+++ daniel.py
@@ -0,0 +1,6 @@
+print("Kelompok         : 31")
+print("Shift            : 2")
+print("Nama Anggota 1   : Michelia Vadilla Verdianto	 (21120120120020)")
+print("Nama Anggota 2   : Donny Ridwan Setiawan (21120120130083)")
+print("Nama Anggota 3   : Muhammad Ghulam Abdul Nashr Umar (21120120120029)")
+print("Nama Anggota 4   : Daniel Andhika Yudistya (21120120140048)")
