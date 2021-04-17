1015350e1afa08b32cb328f4de88a70d60ffbcf8
donnyridwan <82664828+donnyridwan@users.noreply.github.com>
2021-04-17 08:58:07 -0700

    membuat pilihan rute penerbangan dan fungsi book

--- daniel.py
+++ daniel.py
@@ -4,3 +4,16 @@ print("Nama Anggota 1   : Michelia Vadilla Verdianto	 (21120120120020)")
 print("Nama Anggota 2   : Donny Ridwan Setiawan (21120120130083)")
 print("Nama Anggota 3   : Muhammad Ghulam Abdul Nashr Umar (21120120120029)")
 print("Nama Anggota 4   : Daniel Andhika Yudistya (21120120140048)")
+
+print(" ______________________________________ ")
+print("|   SELAMAT DATANG DI TEKKOM AIRWAYS   |")
+print("|______________________________________|")
+print("|         RUTE            |    Harga   |")
+print("|1. JAKARTA-PALANGKARAYA  | Rp 800.000 |")
+print("|2. JAKARTA-SURABAYA      | Rp 650.000 |")
+print("|3. JAKARTA-SEMARANG      | Rp 550.000 |")
+print("|______________________________________|")
+
+def book():
+	pax = int(input("Jumlah Penumpang = "))
+	print ("Masukan nama Penumpang : ")
