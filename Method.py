65b55259ac739a1465f06937479d333a22ced5d3
danielandhika <82562600+danielandhika@users.noreply.github.com>
2021-04-18 16:00:17 +0700

    Menambahkan File Method

---
+++ Method.py
@@ -0,0 +1,11 @@
+class   ticket_method:
+    def __init__(self, ticket):
+        self.ticket = ticket
+
+    def intro(self, kelompok):
+        print(f"Kelompok         : {kelompok} ")
+        print("Shift            : 2")
+        print("Nama Anggota 1   : Michelia Vadilla Verdianto (21120120120020)")
+        print("Nama Anggota 2   : Donny Ridwan Setiawan (21120120130083)")
+        print("Nama Anggota 3   : Muhammad Ghulam Abdul Nashr Umar (21120120120029)")
+        print("Nama Anggota 4   : Daniel Andhika Yudistya (21120120140048)")
